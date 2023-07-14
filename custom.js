637a62700fc498e4005476cc56b3eea41eb2e18a
aporve <60807797+aporve@users.noreply.github.com>
2023-07-14 17:00:33 +0530

    Create custom.js

---
+++ custom.js
@@ -0,0 +1,14 @@
+window.YellowMessengerPlugin.init({host:'https://r2.cloud.yellow.ai',view:'compact',floatingIcon:false})
+document.getElementById("ymDivCircle").style.cssText=`
+bottom: 120px;
+right: 0px !important;
+display: flex !important;
+background-color: #707070 !important;
+border-radius: 0px !important;
+`;document.getElementById("button#ymDivBar").style.cssText=`
+margin-bottom: 120px;
+right: 0px; !important;
+`;document.getElementById("ymFrameHolder").style.cssText=`
+ bottom: 0 !important;
+ right: 0px !important; // added
+`;
